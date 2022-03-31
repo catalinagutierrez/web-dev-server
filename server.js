159067b23acfcc72334b49c2411a27b50e8c278a
Catalina <cgutierrezm95@gmail.com>
2022-03-31 08:40:09 -0400

    Initial commit.

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+
+import helloController from "./controllers/hello-controller.js";
+import userController from "./controllers/users/users-controller.js";
+import tuitsController from "./controllers/tuits/tuits-controller.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+helloController(app);
+userController(app);
+tuitsController(app);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Full Stack Development!");
+});
+
+app.listen(process.env.PORT || 4000);
